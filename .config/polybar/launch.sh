4b2b38fe535ccf1e5434dc67bab1c38d9a245ab7
cohenchris <chris@chriscohen.dev>
2022-07-20 22:25:41 -0700

    backup 07-20-2022_22:25:41

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 res=$(xrandr | grep 'Screen 0' | cut -d , -f 2 | cut -d ' ' -f 5)
-if [ $res -le 1080 ]; then
-  HIDPI=0
-else
-  HIDPI=1
-fi
 
-# Change settings based on whether or not the screen is HIDPI 4k
-if [ $HIDPI -eq 1 ]; then
-  # HIDPI
+if [ $res -gt 1080 ]; then
+  # 1440p
   export BAR_HEIGHT=30
   export UNIFONT="Unifont:size=9:style=Bold;0"
   export FONTAWESOME="FontAwesome:size=14:style=Bold;0"
@@ -18,7 +12,7 @@ if [ $HIDPI -eq 1 ]; then
   export TOP_LEFT="i3 xwindow"
   export TOP_CENTER=" "
 else
-  # Normal
+  # Not 1440p
   export BAR_HEIGHT=25
   export UNIFONT="Unifont:size=8:style=Bold;0"
   export FONTAWESOME="FontAwesome:size=12:style=Bold;0"
@@ -32,8 +26,12 @@ fi
 export BACKGROUND_COLOR=#55000000
 
 # Network Interfaces
-export ETH=$(ifconfig | grep enp | cut -d : -f 1)
-export WLAN=$(ifconfig | grep wlp | cut -d : -f 1)
+export ETH=$(ifconfig | grep enp -m 1 | cut -d : -f 1)
+
+# Only show WiFi if there is no ethernet connection
+if [ -z "${ETH}" ]; then
+  export WLAN=$(ifconfig | grep wlp -m 1 | cut -d : -f 1)
+fi
 
 # Terminate already running bar instances
 killall -q polybar
