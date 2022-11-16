c1dedfb9655b23733eec33075aa5a3b578031cc3
cohenchris <chris@chriscohen.dev>
2022-11-16 14:23:09 -0800

    backup 11-16-2022_14:23:09

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,11 @@
-c.url.searchengines = { 'DEFAULT': 'https://searx.chriscohen.dev/searx/search?q={}&categories=general&language=en-US' }
+c.url.searchengines = { 'DEFAULT': 'https://searx.chriscohen.dev/searx/search?q={}&categories=general&language=en-US',
+                       'aw': 'https://wiki.archlinux.org?search={}',
+                       'yt': 'https://youtube.com/results?search_query={}',
+                       'r': 'https://reddit.com/r/{}' }
+
 config.load_autoconfig()
 config.set("colors.webpage.darkmode.enabled", True)
-config.bind("B", "spawn --userscript qute-bitwarden --dmenu-invocation='dmenu -p \"Select an entry:\" -nb \"#1e1e1e\" -sf \"#aaff77\" -sb \"#333333\" -nf \"#ffffff\" -l 10'")
-config.bind('X', 'hint links userscript add-nextcloud-bookmarks')
+config.bind("B", "spawn --userscript dmenu-bitwarden")
+config.bind("X", "hint links userscript add-nextcloud-bookmarks")
 config.bind("N", "open nextcloud.chriscohen.dev")
 config.bind("P", "open app.plex.tv")
