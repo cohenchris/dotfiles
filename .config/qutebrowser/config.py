5d0df5f9261a913194bbc4206b7fb26fe9a6dc4a
cohenchris <chris@chriscohen.dev>
2023-02-19 17:05:51 -0800

    backup 02-19-2023_17:05:51

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,10 +1,13 @@
 c.url.searchengines = { 'DEFAULT': 'https://searx.chriscohen.dev/searx/search?q={}&categories=general&language=en-US',
                        'aw': 'https://wiki.archlinux.org?search={}',
                        'yt': 'https://youtube.com/results?search_query={}',
-                       'r': 'https://reddit.com/r/{}' }
+                       'r': 'https://reddit.com/r/{}',
+                       'ddg': 'https://duckduckgo.com/?q={}&t=h_&ia=web',
+                       'g': 'https://google.com/search?q={}'}
 
 config.load_autoconfig()
-config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.preferred_color_scheme", "dark")
+config.set("downloads.location.directory", "~/Downloads")
 config.bind("B", "spawn --userscript dmenu-bitwarden")
 config.bind("X", "hint links userscript add-nextcloud-bookmarks")
 config.bind("N", "open nextcloud.chriscohen.dev")
