bd856f21656f6e682f2823a597b1428ff4256e3a
cohenchris <40076937+cohenchris@users.noreply.github.com>
2025-03-30 10:22:08 -0700

    backup 03-30-2025_10:22:08

--- .config/waybar/scripts/speedtest.sh
+++ .config/waybar/scripts/speedtest.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-results=$(speedtest-cli --simple)
+speedtest_cachefile="${XDG_CACHE_HOME}/speedtest_results.txt"
+
+if [[ "$1" == "refresh" || ! -f "${speedtest_cachefile}" ]]; then
+  # If there is no cache file OR the user has explicitly asked to refresh the results, perform a speedtest
+  results=$(speedtest-cli --simple)
+  echo "${results}" > ${speedtest_cachefile}
+else
+  # Use cached results
+  results=$(cat "${speedtest_cachefile}")
+fi
 
 # Extract download and upload speeds using grep and sed
-download=$(echo "$results" | grep '^Download:' | awk '{print int($2)}')
-upload=$(echo "$results" | grep '^Upload:' | awk '{print int($2)}')
+download=$(echo "${results}" | grep '^Download:' | awk '{print int($2)}')
+upload=$(echo "${results}" | grep '^Upload:' | awk '{print int($2)}')
 
 if [[ -z ${upload} || -z ${download} ]]; then
   upload="?"
   download="?"
 fi
 
-echo "󰕒 ${upload} Mb/s / 󰇚 ${download} Mb/s"
+echo "󰇚 ${download} Mb/s / 󰕒 ${upload} Mb/s"
