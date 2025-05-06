8dfcc8fd6d29b4741b800b412a4e79e0267b25f4
cohenchris <40076937+cohenchris@users.noreply.github.com>
2025-05-06 02:14:46 -0700

    backup 05-06-2025_02:14:46

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -2,109 +2,41 @@
 
 # Based on https://github.com/HarHarLinks/wireguard-rofi-waybar/tree/main
 
-# nmcli WireGuard abstraction layer for use with my waybar module and rofi custom menu script
-#
-# requires nmcli on your path
-# install to the same directory as wireguard-rofi.sh
-#
-# usage: ./wireguard.sh [short|menu|toggle NAME]
-# no argument:   print current connections
-# toggle NAME:   toggle connection NAME
+connection_name=""
+connection_ip=""
 
-connected=()
-available=()
+# Check for an active Wireguard VPN connection
+while read -r name uuid type device
+do
+  if [[ ${type} == "wireguard" ]]
+  then
+    while read -r key value
+    do
+      if [[ "${key}" == "ipv4.addresses:" ]]; then
+        connection_name="${name}"
+        connection_ip="${value}"
+      fi
+    done < <(nmcli connection show $name)
+  fi
+done < <(nmcli connection show)
 
-function get_conns {
-	while read -r name uuid type device
-	do
-		if [[ $type != "wireguard" ]]
-		then
-			continue
-		fi
+# Print connection info in JSON format for waybar
+waybar_text=""
+waybar_tooltip=""
 
-		if [[ $device != "--" ]]
-		then
-			while read -r key value
-			do
-				if [[ $key != "ipv4.addresses:" ]]
-				then
-					continue
-				fi
-				connected+=("$name: $value")
-			done < <(nmcli connection show $name)
-		else
-			available+=("$name")
-		fi
-  done < <(nmcli connection show)
-}
-
-function get_vpn_info {
-	local first="yes"
-	local array_print="$1[@]"
-	local array_print=("${!array_print}")
-	local text=""
-	local tooltip=""
-	if [ "${#array_print[@]}" -le 0 ]
-	then
-		return
-	fi
-
-  for c in "${array_print[@]}"
-  do
-    if [[ "$first" != "yes" ]]
-    then
-        text="$text | "
-        tooltip="$tooltip\n"
-    fi
-    if [[ "$2" == "short" ]]
-    then
-      text="$text$(echo -n $c | sed -E 's/^(.+): ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]+)$/\1/')"
-    else
-      text="$text$c"
-    fi
-    tooltip="$tooltip$c"
-    first="no"
-  done
-  echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}"
-}
-
-function array_contains {
-	local array_has="$1[@]"
-	local array_has=("${!array_has}")
-	local element="$2"
-	for e in "${array_has[@]}"
-	do
-		if [[ "$e" == *"$element"* ]]
-		then
-			echo "yes"
-			return
-		fi
-	done
-	echo "no"
-}
-
-function toggle_vpn_connection()
-{
-	get_conns
-
-	if [[ "$(array_contains connected $conn)" == "yes" ]]
-	then
-		nmcli connection down "$conn"
-	elif [[ "$(array_contains available $conn)" == "yes" ]]
-	then
-		nmcli connection up "$conn"
-	else
-		echo "err: connection not found"
-		exit 1
-	fi
-}
-
-# Parse and handle arguments
-if [ "$1" == "info" ]; then
-  get_vpn_info connected
-elif [ "$1" == "toggle" ]; then
-  toggle_vpn_connection "$2"
+if [[ "${connection_name}" == "" ]]; then
+  connection_icon="󱦚  "
+  connection_status="Disconnected"
+  connection_tooltip_status="<span color='#c84b4b'>${connection_status}</span>"
+  connection_name="N/A"
+  connection_ip="N/A"
 else
-  echo "Invalid argument - choose one of [info, toggle]."
-  exit 1
+  connection_icon="󰦝  "
+  connection_status="Connected"
+  connection_tooltip_status="<span color='#00c000'>${connection_status}</span>"
 fi
+
+waybar_text="${connection_icon}VPN ${connection_status}"
+waybar_tooltip="<big>Wireguard VPN ${connection_tooltip_status}</big>\n\nConnection Name\t${connection_name}\nIP Address\t${connection_ip}"
+
+echo "{\"text\": \"${waybar_icon}${waybar_text}\", \"tooltip\": \"${waybar_tooltip}\"}"
