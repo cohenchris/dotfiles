6d90041fbcda8abcae5b1176e33c8f55094cd26d
cohenchris <40076937+cohenchris@users.noreply.github.com>
2025-05-07 17:44:43 -0700

    backup 05-07-2025_17:44:43

--- .config/waybar/scripts/waybar.sh
+++ .config/waybar/scripts/waybar.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if [ -e "/sys/class/power_supply" ]; then
-  # No battery information means we're on a desktop
-  SYSTEM_TYPE="desktop"
-else
-  # Battery information means we're on a laptop
-  SYSTEM_TYPE="laptop"
-fi
-
-hyprctl dispatch exec "waybar -c ${XDG_CONFIG_HOME}/waybar/${SYSTEM_TYPE}/top.jsonc"
-hyprctl dispatch exec "waybar -c ${XDG_CONFIG_HOME}/waybar/${SYSTEM_TYPE}/bottom.jsonc"
+hyprctl dispatch exec "waybar -c ${XDG_CONFIG_HOME}/waybar/top.jsonc"
+hyprctl dispatch exec "waybar -c ${XDG_CONFIG_HOME}/waybar/bottom.jsonc"
