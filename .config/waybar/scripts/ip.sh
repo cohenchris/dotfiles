78a168c5c2de80f5951b20ae5206f65420004141
cohenchris <40076937+cohenchris@users.noreply.github.com>
2026-01-04 13:34:14 -0800

    backup 01-04-2026_13:34:13

--- .config/waybar/scripts/ip.sh
+++ .config/waybar/scripts/ip.sh
@@ -3,12 +3,11 @@
 # Get external IP address using curl
 external_ip=$(dig +short txt ch whoami.cloudflare @1.0.0.1)
 
+# Handle external IP dig failure gracefully
+[[ $? -ne 0 ]] && external_ip="?"
+
 # Remove quotes from external IP string
 external_ip=${external_ip//\"/}
-
-if [[ "${external_ip}" == "" ]]; then
-  external_ip="?"
-fi
 
 # Get internal IP addresses (assuming you want to display the first non-loopback interface)
 internal_ip=$(hostname -i | awk '{print $1}')
