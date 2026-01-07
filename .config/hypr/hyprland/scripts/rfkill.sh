22ef4f6a1ef4cb7b31a819e6ef4e7306595fc29a
cohenchris <40076937+cohenchris@users.noreply.github.com>
2026-01-06 19:53:10 -0800

    backup 01-06-2026_19:53:09

--- .config/hypr/hyprland/scripts/rfkill.sh
+++ .config/hypr/hyprland/scripts/rfkill.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+rfkill --output TYPE,SOFT | grep wlan
+has_wlan=$?
 
-rfkill list | grep -q "Soft blocked: no"
-is_blocked=$?
-
-if [[ "${is_blocked}" -eq 0 ]]; then
-  notify CRITICAL "󰀝  Airplane mode enabled" "Network connectivity killed"
-  rfkill block all
-else
-  notify GOOD "󰀝  Airplane mode disabled" "Network connectivity restored"
-  rfkill unblock all
+if [[ "${has_wlan}" -eq 1 ]]; then
+  # No WLAN device, continue
+  notify CRITICAL "󰀝  No WLAN device found" "Cannot toggle airplane mode"
+  exit 1
 fi
+
+rfkill --output TYPE,SOFT | grep wlan | grep unblocked
+is_unblocked=$?
+
+if [[ "${is_unblocked}" -eq 0 ]]; then
+  notify GOOD "󰀝  Airplane mode disabled" "Network connectivity restored"
+else
+  notify CRITICAL "󰀝  Airplane mode enabled" "Network connectivity killed"
+fi
+
+rfkill toggle wlan
